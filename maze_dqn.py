af4f3f46cb5aa170256417bee35305e38f99424e
plarotta <plarottaone2@gmail.com>
2023-11-04 20:18:53 -0400

    cooked up a dqn implementation based on the atari dqn. did not test yet because am having torch issues on my local machine :(

--- maze_dqn.py
+++ maze_dqn.py
@@ -5,8 +5,16 @@ import gymnasium as gym
 import miniworld
 from matplotlib import pyplot as plt
 from PIL import Image
+import random
 
+from copy import copy
+from itertools import count
+from collections import deque
+from utilities.wrappers import WarpFrame, PyTorchFrame
+from utilities.agent import DQNAgent
+from utilities.memory import ReplayBuffer
 
+import torch
 
 env = gym.make("MiniWorld-Maze-v0", 
                num_rows=10, 
@@ -14,20 +22,90 @@ env = gym.make("MiniWorld-Maze-v0",
                room_size=5, 
                render_mode='human',
                view='top')
-env.reset()
 
-initial_action = env.action_space.sample()
-print(f'action space: {env.action_space}')
-print(f'obs space: {env.observation_space}')
-print(f'random act: {initial_action}')
+env = WarpFrame(env) # RGB to Grayscale
+env = PyTorchFrame(env)
+print("initialized environment...")
 
-obs, _, _, _, _ = env.step(initial_action)
-print(obs.shape)
+replay_buffer = ReplayBuffer(5000)
+print("initialized replay buffer...")
 
-# obs[0:256, 0:256] = [255, 0, 0] # red patch in upper left
-img = Image.fromarray(obs, 'RGB')
-img.save('my.png')
-img.show()
+
+agent = DQNAgent(
+    env.observation_space,
+    env.action_space,
+    replay_buffer,
+    lr=1e-4,
+    batch_size=32,
+    gamma=0.99,
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+)
+print("initialized dqn agent...")
+
+
+eps_timesteps = 0.1* \
+    int(1e6)
+episode_rewards = [0.0]
+
+state, _ = env.reset()
+
+
+for t in range(1):
+    print(f'training episode step {t}')
+    fraction = min(1.0, float(t) / eps_timesteps)
+    eps_threshold = 1 + fraction * \
+        (0.01 - 1)
+    sample = random.random()
+
+    if(sample > eps_threshold):
+        # Exploit
+        action = agent.act(state)
+    else:
+        # Explore
+        action = env.action_space.sample()
+
+    next_state, reward, done, info = env.step(action)
+    agent.memory.add(state, action, reward, next_state, float(done))
+    state = next_state
+
+    episode_rewards[-1] += reward
+    if done:
+        state = env.reset()
+        episode_rewards.append(0.0)
+
+    if t > 1000 and t % 1 == 0:
+        agent.optimise_td_loss()
+
+    if t > 1000 and t % 100 == 0:
+        agent.update_target_network()
+
+    num_episodes = len(episode_rewards)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# initial_action = env.action_space.sample()
+# print(f'action space: {env.action_space}')
+# print(f'obs space: {env.observation_space}')
+# print(f'random act: {initial_action}')
+
+# obs, _, _, _, _ = env.step(initial_action)
+# print(obs.shape, type(obs))
+
+# img = Image.fromarray(obs.squeeze(), mode="L")
+# img.save('my.png')
+# img.show()
 
 
 
---
+++ utilities/agent.py
@@ -0,0 +1,97 @@
+# Inspired from https://github.com/KaleabTessera/DQN-Atari/
+from gym import spaces
+import numpy as np
+
+from utilities.dqn_atari_model import DQN
+from utilities.memory import ReplayBuffer
+import torch
+import torch.nn.functional as F
+
+
+class DQNAgent:
+    def __init__(self,
+                 observation_space: spaces.Box,
+                 action_space: spaces.Discrete,
+                 replay_buffer: ReplayBuffer,
+                 lr,
+                 batch_size,
+                 gamma,
+                 device=torch.device("cpu" )):
+        """
+        Initialise the DQN algorithm using the Adam optimiser
+        :param action_space: the action space of the environment
+        :param observation_space: the state space of the environment
+        :param replay_buffer: storage for experience replay
+        :param lr: the learning rate for Adam
+        :param batch_size: the batch size
+        :param gamma: the discount factor
+        """
+
+        self.memory = replay_buffer
+        self.batch_size = batch_size
+        self.gamma = gamma
+
+        self.policy_network = DQN(observation_space, action_space).to(device)
+        self.target_network = DQN(observation_space, action_space).to(device)
+        self.update_target_network()
+        self.target_network.eval()
+
+        # self.optimiser = torch.optim.RMSprop(self.policy_network.parameters()
+        #     , lr=lr)        
+        self.optimiser = torch.optim.Adam(self.policy_network.parameters(), lr=lr)
+
+        self.device = device
+
+    def optimise_td_loss(self):
+        """
+        Optimise the TD-error over a single minibatch of transitions
+        :return: the loss
+        """
+        device = self.device
+
+        states, actions, rewards, next_states, dones = self.memory.sample(self.batch_size)
+        states = np.array(states) / 255.0
+        next_states = np.array(next_states) / 255.0
+        states = torch.from_numpy(states).float().to(device)
+        actions = torch.from_numpy(actions).long().to(device)
+        rewards = torch.from_numpy(rewards).float().to(device)
+        next_states = torch.from_numpy(next_states).float().to(device)
+        dones = torch.from_numpy(dones).float().to(device)
+
+        with torch.no_grad():
+            _, max_next_action = self.policy_network(next_states).max(1)
+            max_next_q_values = self.target_network(next_states).gather(1, max_next_action.unsqueeze(1)).squeeze()
+
+            target_q_values = rewards + (1 - dones) * self.gamma * max_next_q_values
+
+        input_q_values = self.policy_network(states)
+        input_q_values = input_q_values.gather(1, actions.unsqueeze(1)).squeeze()
+
+        loss = F.smooth_l1_loss(input_q_values, target_q_values)
+
+        self.optimiser.zero_grad()
+        loss.backward()
+        self.optimiser.step()
+        del states
+        del next_states
+        return loss.item()
+
+    def update_target_network(self):
+        """
+        Update the target Q-network by copying the weights from the current Q-network
+        """
+        self.target_network.load_state_dict(self.policy_network.state_dict())
+
+    def act(self, state: np.ndarray):
+        """
+        Select an action greedily from the Q-network given the state
+        :param state: the current state
+        :return: the action to take
+        """
+        device = self.device
+        state = np.array(state) / 255.0
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        with torch.no_grad():
+            q_values = self.policy_network(state)
+            _, action = q_values.max(1)
+            return action.item()
