2ef74eb00cb1b2d0c6030a6e094ef53b9ba0fbcf
Aymeric <aymeric.degroote@gmail.com>
2023-12-03 02:27:07 -0500

    minor changes to maze_gym, forgot to commit it earlier

--- maze_gym.py
+++ maze_gym.py
@@ -4,18 +4,18 @@ https://minigrid.farama.org/content/create_env_tutorial/
 
 And integrated mazelib to generate the maze
 
-TODO: remove this file that is pretty useless
+TODO: rename this file as minigreedy.py
 """
 
 from __future__ import annotations
 
 from minigrid_utilities.environment import MiniGridMazeEnv
-from minigrid_utilities.policymaker import Agent
+from minigrid_utilities.policymaker import Agent, MiniGridAgent
 
 
 def main():
 
-    num_episodes = 2
+    num_episodes = 3
     max_num_step = 1000
 
     env = MiniGridMazeEnv(render_mode="human",
@@ -23,27 +23,23 @@ def main():
                           )
 
     observation, info = env.reset(seed=0)
-    observation = observation.get('image')[:,:,0]
-    # TODO: either integrate [:,:,0] in reset function or use the whole image
-    # Doesn't really matter since we will switch to MiniWorld at some point
 
     obs_space_dims = observation.shape
     action_space_dims = 3
 
     for ep_id in range(num_episodes):
         observation, info = env.reset(seed=ep_id)
-        observation = observation.get('image')[:,:,0]
 
-        agent = Agent(obs_space_dims, action_space_dims,
-                      size=env.size, load_maze=True,
-                      policy="greedy"
+        agent = MiniGridAgent(obs_space_dims, action_space_dims,
+                              size=env.size, load_maze=True,
+                              policy="greedy",
+                              network=False
                      )
 
         for i in range(max_num_step):
             action = agent.policy(observation)
 
             observation, reward, terminated, truncated, info = env.step(action)
-            observation = observation.get('image')[:,:,0]
             agent.rewards.append(reward)
 
             # Reward system:
